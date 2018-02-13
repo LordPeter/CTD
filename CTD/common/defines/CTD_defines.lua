457968aee439ff0cceb86fbc58ac74af27eca093
LordPeter <LordPeter@users.noreply.github.com>
2018-02-13 23:35:07 +0100

    INITIALIZE: Add mod contents as of legacy version 1.5.0+

---
+++ CTD/common/defines/CTD_defines.lua
@@ -0,0 +1,9 @@
+--Reduce Tribal Empty Holding Slot bonus to account for population system
+NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_LEVY_MULTIPLIER = 0.05			-- Only get 5% bonus each, down from 50%
+NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_GARRISON_MULTIPLIER = 0.05		-- Only get 5% bonus each, down from 50%
+NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_TAX_MULTIPLIER = 0.05			-- Only get 5% bonus each, down from 50%
+
+--Change graphical stuff
+NDefines.NGraphics.TREASURY_MODIFIER_ICON_FRAME = 138	-- default: 13
+NDefines.NGraphics.CITY_MAIN_BUILDING_LEVEL_2 = 35		-- default: 25
+NDefines.NGraphics.CITY_MAIN_BUILDING_LEVEL_3 = 50		-- default: 35
