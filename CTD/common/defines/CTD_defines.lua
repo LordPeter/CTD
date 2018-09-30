ad487979ceb8097d919aa4fd68ba261be30d19b8
LordPeter <lord.peter.ii@gmx.de>
2018-09-30 14:14:29 +0200

    TWEAK: More detail to Nomad Pops
    But there still should be some other pops in Nomad capitals! Todo.

--- CTD/common/defines/CTD_defines.lua
+++ CTD/common/defines/CTD_defines.lua
@@ -1,7 +1,8 @@
---Reduce Tribal Empty Holding Slot bonus to account for population system
-NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_LEVY_MULTIPLIER = 0.05			-- Only get 5% bonus each, down from 50%
-NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_GARRISON_MULTIPLIER = 0.05		-- Only get 5% bonus each, down from 50%
-NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_TAX_MULTIPLIER = 0.05			-- Only get 5% bonus each, down from 50%
+--Remove Various Empty Holding Slot boni to account for population system
+NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_LEVY_MULTIPLIER = 0		-- No bonus (originally 50%)
+NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_GARRISON_MULTIPLIER = 0	-- No bonus (originally 50%)
+NDefines.NCharacter.TRIBAL_EMPTY_HOLDING_TAX_MULTIPLIER = 0			-- No bonus (originally 50%)
+NDefines.NNomad.MAX_POPULATION_EMPTY_HOLDING_MULTIPLIER = 0			-- No bonus (originally 1000)
 
 --Change graphical stuff
 NDefines.NGraphics.TREASURY_MODIFIER_ICON_FRAME = 138	-- default: 13
